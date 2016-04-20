7a2121dfdc5689a90e2c5915c74d4ee316b295e4
Landon Reed <landontreed@gmail.com>
2016-04-20 11:43:20 -0400

    deleted unneeded actions/reducers and moved permissions util to common

--- src/main/client/common/util/permissions.js
+++ src/main/client/common/util/permissions.js
@@ -8,8 +8,9 @@ export const getFeedsForPermission = (project, user, permission) => {
 export const checkEntitiesForFeeds = (entities, feeds) => {
   // if (!entities) return true
   // console.log(entities)
+  // console.log(feeds)
   let publishableIds = feeds.map(f => f.id)
-  let entityIds = entities.map(e => e.agency.id)
+  let entityIds = entities.map(e => e.agency ? e.agency.id : null)
   for (var i = 0; i < entityIds.length; i++) {
     if (publishableIds.indexOf(entityIds[i]) === -1) return false
   }
