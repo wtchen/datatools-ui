8174fe094d4bb46a726edad704c064b0a41d0269
Landon Reed <landontreed@gmail.com>
2016-06-10 14:49:40 -0400

    update webpack for font awesome

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,14 @@ module.exports = {
       //   test: /\.css$/,
       //   loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
       },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
       // css-loader
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=10000'},
