a1a533c8523bd58b3d37af903a9383f6b00c9772
Landon Reed <landontreed@gmail.com>
2016-07-08 13:38:27 -0400

    use array of points for routing

--- src/main/client/scenario-editor/utils/valhalla.js
+++ src/main/client/scenario-editor/utils/valhalla.js
@@ -1,30 +1,40 @@
 import fetch from 'isomorphic-fetch'
 import {decode as decodePolyline} from 'polyline'
 
-export async function route (start, end) {
+export async function route (points) {
+  if (points.length < 2) {
+    return null
+  }
   const MAPZEN_TURN_BY_TURN_KEY = DT_CONFIG['MAPZEN_TURN_BY_TURN_KEY']
+  const locations = points.map(p => (
+    {lon: p.lng, lat: p.lat}
+  ))
   const json = {
     costing: 'bus',
-    locations: [
-      {lon: start.lng, lat: start.lat},
-      {lon: end.lng, lat: end.lat}
-    ]
+    locations
   }
   const response = await fetch(`https://valhalla.mapzen.com/route?json=${JSON.stringify(json)}&api_key=${MAPZEN_TURN_BY_TURN_KEY}`)
   return await response.json()
 }
 
-export async function polyline (start, end) {
-  const json = await route(start, end)
-  const maneuvers = json.trip.legs[0].maneuvers
-  let ignorePoints = {}
-  for (var i = 0; i < maneuvers.length; i++) {
-    if (maneuvers[i].type === 5 || maneuvers[i].type === 6) {
-      ignorePoints.from = maneuvers[i].begin_shape_index
-      ignorePoints.to = maneuvers[i].end_shape_index
-    }
+export async function polyline (points) {
+  let json
+  try {
+    json = await route(points)
+  } catch (e) {
+    return null
   }
-  return decodePolyline(json.trip.legs[0].shape)
-    .filter((c, index) => !(index === ignorePoints.from || index === ignorePoints.to)) // filter out points used for destination on right/left
-    .map((c, index) => [c[1] / 10, c[0] / 10]) // Mapzen or Mapbox is encoding/decoding wrong?
+  const legArray = json.trip.legs.map((leg, index) => {
+    let ignorePoints = {}
+    for (var i = 0; i < leg.maneuvers.length; i++) {
+      if (leg.maneuvers[i].type === 5 || leg.maneuvers[i].type === 6) {
+        ignorePoints.from = leg.maneuvers[i].begin_shape_index
+        ignorePoints.to = leg.maneuvers[i].end_shape_index
+      }
+    }
+    return decodePolyline(leg.shape)
+      .filter((c, index) => !(index === ignorePoints.from || index === ignorePoints.to)) // filter out points used for destination on right/left
+      .map((c, index) => [c[1] / 10, c[0] / 10]) // Mapzen or Mapbox is encoding/decoding wrong?
+  })
+  return [].concat.apply([], legArray)
 }
