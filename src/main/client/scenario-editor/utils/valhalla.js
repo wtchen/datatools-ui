b6c13ae5c9819e3093ebf58efa7d14994835d41c
Landon Reed <landontreed@gmail.com>
2016-07-20 09:21:51 -0400

    handle null json response

--- src/main/client/scenario-editor/utils/valhalla.js
+++ src/main/client/scenario-editor/utils/valhalla.js
@@ -24,17 +24,22 @@ export async function polyline (points) {
   } catch (e) {
     return null
   }
-  const legArray = json.trip.legs.map((leg, index) => {
-    let ignorePoints = {}
-    for (var i = 0; i < leg.maneuvers.length; i++) {
-      if (leg.maneuvers[i].type === 5 || leg.maneuvers[i].type === 6) {
-        ignorePoints.from = leg.maneuvers[i].begin_shape_index
-        ignorePoints.to = leg.maneuvers[i].end_shape_index
+  if (json) {
+    const legArray = json.trip.legs.map((leg, index) => {
+      let ignorePoints = {}
+      for (var i = 0; i < leg.maneuvers.length; i++) {
+        if (leg.maneuvers[i].type === 5 || leg.maneuvers[i].type === 6) {
+          ignorePoints.from = leg.maneuvers[i].begin_shape_index
+          ignorePoints.to = leg.maneuvers[i].end_shape_index
+        }
       }
-    }
-    return decodePolyline(leg.shape)
-      .filter((c, index) => !(index === ignorePoints.from || index === ignorePoints.to)) // filter out points used for destination on right/left
-      .map((c, index) => [c[1] / 10, c[0] / 10]) // Mapzen or Mapbox is encoding/decoding wrong?
-  })
-  return [].concat.apply([], legArray)
+      return decodePolyline(leg.shape)
+        .filter((c, index) => !(index === ignorePoints.from || index === ignorePoints.to)) // filter out points used for destination on right/left
+        .map((c, index) => [c[1] / 10, c[0] / 10]) // Mapzen or Mapbox is encoding/decoding wrong?
+    })
+    return [].concat.apply([], legArray)
+  }
+  else {
+    return null
+  }
 }
