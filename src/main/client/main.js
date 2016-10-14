d6c565d09bf267ae442f5f91b76e5e2473327b10
Landon Reed <landontreed@gmail.com>
2016-10-14 10:43:06 -0400

    code style (use window)

--- src/main/client/main.js
+++ src/main/client/main.js
@@ -28,7 +28,10 @@ var lang = requireAll(require.context('json!yaml!../../../i18n', true, /.yml/))
 // is an array containing all the matching modules
 config.messages = {}
 config.messages.all = lang
-const languageId = window.localStorage.getItem('lang') ? localStorage.getItem('lang') : navigator.language || navigator.userLanguage
+const languageId = window.localStorage.getItem('lang')
+  ? window.localStorage.getItem('lang')
+  : navigator.language || navigator.userLanguage
+
 config.messages.active = lang.find(l => l.id === languageId) || lang.find(l => l.id === 'en-US')
 
 console.log('config', config)
