ed3e93a40a7af5c06cf2702879be9b54e65ef2d6
Landon Reed <landontreed@gmail.com>
2017-02-01 11:49:19 -0500

    fix(TimetableEditor): fix offset for new trips
    offset was not applying to newly added trips, due to referencing code that had been refactored
    
    (offset moved from this.state to this.props.timetable)
    
    #22

--- src/main/client/editor/components/timetable/TimetableEditor.js
+++ src/main/client/editor/components/timetable/TimetableEditor.js
@@ -44,7 +44,7 @@ export default class TimetableEditor extends Component {
 
     // set starting time for first arrival
     let cumulativeTravelTime = !toClone ? 0 : objectPath.get(newRow, `stopTimes.0.arrivalTime`)
-    cumulativeTravelTime += this.state.offsetSeconds
+    cumulativeTravelTime += this.props.timetable.offset
 
     for (let i = 0; i < activePattern.patternStops.length; i++) {
       let stop = activePattern.patternStops[i]
@@ -80,13 +80,13 @@ export default class TimetableEditor extends Component {
   addNewRow (blank = false, scroll = false) {
     // set blank to true if there are no rows to clone
     blank = blank || this.props.timetable.trips.length === 0
-
-    let clone = blank ? null : this.props.timetable.trips[this.props.timetable.trips.length - 1]
+    const lastIndex = this.props.timetable.trips.length - 1
+    let clone = blank ? null : this.props.timetable.trips[lastIndex]
     let newRow = this.constructNewRow(clone)
 
     let stateUpdate = {
       activeCell: {$set: null},
-      scrollToRow: {$set: this.props.timetable.trips.length},
+      scrollToRow: {$set: lastIndex + 1}, // increment selected row
       scrollToColumn: {$set: 0}
     }
     this.props.addNewTrip(newRow)
