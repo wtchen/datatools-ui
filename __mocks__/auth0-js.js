fa6d375ef336bdff27dc101277bc160cd88d3f21
Evan Siroky <evan.siroky@yahoo.com>
2017-07-21 18:54:32 -0700

    refactor(auth): refactor auth0manager
    - make things more modular to save code
    - add tests for auth stuff

---
+++ __mocks__/auth0-js.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+
+module.exports = {
+  WebAuth: class WebAuth {
+    constructor ({domain, clientID}) {
+      if (!domain) {
+        throw new Error('Domain required')
+      }
+      if (!clientID) {
+        throw new Error('Client ID required')
+      }
+    }
+
+    renewAuth (
+      {
+        audience,
+        nonce,
+        postMessageDataType,
+        redirectUri,
+        scope,
+        usePostMessage
+      },
+      callback
+    ) {
+      return callback(null, {
+        idToken: jwt.sign(
+          {
+            nonce
+          },
+          'signingKey'
+        )
+      })
+    }
+  }
+}
---
+++ __mocks__/auth0-lock.js
@@ -0,0 +1,23 @@
+export default function Auth0Lock (clientID, domain, options) {
+  this.options = options
+}
+
+Auth0Lock.prototype.getProfile = function (token, callback) {
+  if (this.options.getProfileSuccess) {
+    callback(null, this.options.getProfileResult)
+  } else {
+    callback(new Error('error obtaining Auth0 profile'))
+  }
+}
+
+Auth0Lock.prototype.hide = function () {}
+
+Auth0Lock.prototype.on = function (event, next) {
+  next({ idToken: this.options.fakeAuthenticatedToken })
+}
+
+Auth0Lock.prototype.setOptions = function (options) {
+  this.options = options
+}
+
+Auth0Lock.prototype.show = function () {}
