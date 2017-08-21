1a60200efc8cd38d8f9e326038d57576f44fdc8a
Evan Siroky <evan.siroky@yahoo.com>
2017-08-21 15:48:34 -0700

    test(flow): Add flow typing to lib/common/util
    Also, replace lib/common/util/to-sentence-case with npm package
    to-sentence-case

---
+++ flow-typed/npm/to-sentence-case_v1.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: a452fa6b644833cb3d122478900a5b7c
+// flow-typed version: <<STUB>>/to-sentence-case_v1.0.0/flow_v0.53.1
+
+declare module 'to-sentence-case' {
+  declare export default (string) => string
+}
