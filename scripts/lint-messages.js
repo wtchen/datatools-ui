ba8871dc8b9538f96a4e0ace4aa5180dc0c79ce7
evansiroky <evan.siroky@yahoo.com>
2018-09-21 13:40:18 -0700

    refactor(messages): add more comments and stats reporting to message linting

--- scripts/lint-messages.js
+++ scripts/lint-messages.js
@@ -1,18 +1,39 @@
 // @flow
 
+console.time('lint-messages')
+
 const fs = require('fs')
 const path = require('path')
 
 const safeLoad = require('js-yaml').safeLoad
 const objectPath = require('object-path')
 
-let atLeastOneErrorFound = false
+const issueTypes = {
+  languageMessageSimilarity: 'warning',
+  unusedMessageDefinition: 'warning',
+  undefinedMessage: 'error',
+  dynamicMessageWithoutPossibilitiesComment: 'warning'
+}
+const error = []
+const jsMessageCounts = {}
+const languageMessageCounts = {}
+const warning = []
+
+function addIssue (type, issue) {
+  const issueErrorLevel = issueTypes[type]
+  console[issueErrorLevel === 'warning' ? 'warn' : 'error'](`${issueErrorLevel.toUpperCase()}: ${issue}`)
+  if (issueErrorLevel === 'error') {
+    error.push({ issue, type })
+  } else {
+    warning.push({ issue, type })
+  }
+}
+
 const foundMessages = {}
 const messages = {}
 
 // read in language data
-const languages = fs.readdirSync('i18n')
-languages.forEach(langFile => {
+fs.readdirSync('i18n').forEach(langFile => {
   // $FlowFixMe not sure why there's an error here
   messages[langFile] = safeLoad(fs.readFileSync(path.join('i18n', langFile)))
 })
@@ -33,7 +54,7 @@ function exploreDir (filePath) {
 }
 
 /**
- * Analyze a .js file to check for messages
+ * Analyze a .js file to keep track of messages found within .js code
  */
 function analyzeFile (filePath) {
   const classRegex = /class (\w*) extends MessageComponent/gm
@@ -55,18 +76,24 @@ function analyzeFile (filePath) {
       const messageIndex = line.indexOf('this.messages(')
       if (messageIndex > -1) {
         // determine what the message path is
-        // if the first character is not ' or ` it's a variable
+        // if the first character is not ', it's a variable or a template string
         const firstChar = line[messageIndex + 14]
         if (firstChar === "'") {
-          // isolate string so regex doesn't pick up anything before
+          // found a parseable message, add some stats
+          jsMessageCounts[filePath] = (jsMessageCounts[filePath] || 0) + 1
+
+          // argument sent to this.messages is a static string, get the string
+          // and add an entry to the foundMessages object
           const message = line.substring(
             messageIndex + 15,
             line.indexOf(firstChar, messageIndex + 16)
           )
           objectPath.set(foundMessages, `${curClass}.${message}`, true)
         } else {
+          // argument sent to this.messages is a variable or string template
           // TODO: analyze some kind of comment outlining message possibilities
-          console.warn(
+          addIssue(
+            'dynamicMessageWithoutPossibilitiesComment',
             `unable to determine message object path from variable or template string in file ${filePath} on line ${lineIdx}`
           )
         }
@@ -75,17 +102,51 @@ function analyzeFile (filePath) {
   })
 }
 
-// iterate through files and tally up what was found and what wasn't
+// iterate through js files and find all places where messages are used
 exploreDir('lib')
 
-// analyze differences of messages in language files
+const seenDefinedMessages = {}
+
+// explore message definitions in all languages and do two things for each
+// message value:
+// 1. Check if the messages defined in the language files are used in any js
+//    file.  If not, they do not need to exist in the language file.
+// 2. Analyze message similarity across language files. Each language should
+//    probably differ slightly from the other, but maybe not when a message in
+//    one language file is the same as a message in another language file,
+//    create a warning issue
 Object.keys(messages).forEach(langFile => {
   const langMessages = messages[langFile]
+  languageMessageCounts[langFile] = 0
 
-  function reconcileObject (obj, pathSoFar) {
+  // this function is used to recursively explore the next level of an object in
+  // a message object
+  function analyzeMessageObject (obj, pathSoFar) {
+    // iterate through all keys of the current object
     Object.keys(obj).forEach(key => {
       const nextPath = pathSoFar ? `${pathSoFar}.${key}` : key
       if (typeof obj[key] === 'string') {
+        // the value of this key is a string, therefore, the key/value pair is a
+        // message
+        languageMessageCounts[langFile]++
+
+        // check if this message was found in any js files in the project
+        if (!objectPath.get(foundMessages, nextPath)) {
+          // FIXME: make this cause an error once variables and
+          // template strings are accounted for
+          addIssue(
+            'unusedMessageDefinition',
+            `message ${nextPath} in ${langFile} not found in any .js files`
+          )
+        }
+
+        // don't bother anlayzing the language similarity if we've already seen
+        // this message in another language file
+        if (seenDefinedMessages[nextPath]) return
+        seenDefinedMessages[nextPath] = true
+
+        // analyze the similarity of this message in this language file to the
+        // corresponding message in other language message files
         Object.keys(messages)
           .filter(compareLangFile => compareLangFile !== langFile)
           .forEach(compareLangFile => {
@@ -93,62 +154,69 @@ Object.keys(messages).forEach(langFile => {
             if (obj[key] === objectPath.get(compareLangMessages, nextPath)) {
               // FIXME: make this cause an error once we decide to have actual
               // translations for all languages
-              console.warn(`WARNING: message ${nextPath} in ${langFile} is the same as the message in ${compareLangFile}`)
+              addIssue(
+                'languageMessageSimilarity',
+                `message ${nextPath} in ${langFile} is the same as the message in ${compareLangFile}`
+              )
             }
           })
       } else {
-        reconcileObject(obj[key], nextPath)
+        analyzeMessageObject(obj[key], nextPath)
       }
     })
   }
 
-  reconcileObject(langMessages['components'])
+  analyzeMessageObject(langMessages['components'])
 })
 
-// analyze what defined messages in the language files were found and which were not
-Object.keys(messages).forEach(langFile => {
-  const langMessages = messages[langFile]
-
-  function reconcileObject (obj, pathSoFar) {
-    Object.keys(obj).forEach(key => {
-      const nextPath = pathSoFar ? `${pathSoFar}.${key}` : key
-      if (typeof obj[key] === 'string') {
-        if (!objectPath.get(foundMessages, nextPath)) {
-          // FIXME: make this cause an error once variables and
-          // template strings are accounted for
-          console.warn(`WARNING: message ${nextPath} in ${langFile} not found in any .js files`)
-        }
-      } else {
-        reconcileObject(obj[key], nextPath)
-      }
-    })
-  }
-
-  reconcileObject(langMessages['components'])
-})
-
-// analyze which messages found in the .js files were found or not in the language files
-function reconcileFoundObject (obj, pathSoFar) {
+// Recursively analyze a foundMessages Object to determine which messages that
+// were found in the .js files have a corresponding entry in each language file
+function analyzeFoundObject (obj, pathSoFar) {
   Object.keys(obj).forEach(key => {
     const nextPath = pathSoFar ? `${pathSoFar}.${key}` : key
     if (typeof obj[key] === 'boolean') {
+      // the value of this key is a boolean, therefore, the key/value pair
+      // indicates a message found in a js file
       Object.keys(messages).forEach(langFile => {
         const langMessages = messages[langFile]['components']
         if (!objectPath.get(langMessages, nextPath)) {
-          atLeastOneErrorFound = true
-          console.error(`ERROR: message ${nextPath} not defined in language file ${langFile}`)
+          addIssue(
+            'undefinedMessage',
+            `message ${nextPath} not defined in language file ${langFile}`
+          )
         }
       })
     } else {
-      reconcileFoundObject(obj[key], nextPath)
+      analyzeFoundObject(obj[key], nextPath)
     }
   })
 }
 
-reconcileFoundObject(foundMessages)
+analyzeFoundObject(foundMessages)
 
-if (atLeastOneErrorFound) {
+// print results
+console.log('')
+console.timeEnd('lint-messages')
+console.log(`\nFound ${Object.keys(languageMessageCounts).length} message files.`)
+Object.keys(languageMessageCounts).forEach(langFile => {
+  console.log(` - ${langFile}: ${languageMessageCounts[langFile]} messages`)
+})
+console.log(`\nFound ${
+  Object.keys(jsMessageCounts).length
+} js files with ${
+  Object.keys(jsMessageCounts).reduce((accumulator, currentValue) => {
+    return accumulator + jsMessageCounts[currentValue]
+  }, 0)
+} total messages.`)
+console.log(`\nLinting produced ${error.length} errors and ${warning.length} warnings.`)
+Object.keys(issueTypes).forEach(issueType => {
+  console.log(` - ${issueType}: ${error.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue.type === issueType ? 1 : 0)
+  }, 0)} errors and ${warning.reduce((accumulator, currentValue) => {
+    return accumulator + (currentValue.type === issueType ? 1 : 0)
+  }, 0)} warnings`)
+})
+
+if (error.length > 0) {
   process.exit(2)
-} else {
-  console.log('no errors found with messages!')
 }
