1ddf44efca74ed2f733e9d8620a132f69eff6d41
Evan Siroky <evan.siroky@yahoo.com>
2019-08-22 11:42:40 -0700

    tests(e2e): only run e2e tests on Travis on dev or master branches

--- scripts/check-if-e2e-should-run-on-travis.sh
+++ scripts/check-if-e2e-should-run-on-travis.sh
@@ -1,13 +1,3 @@
-if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
-  if [[ "$TRAVIS_PULL_REQUEST_SLUG" = "conveyal/datatools-ui" ]]; then
-    export SHOULD_RUN_E2E=true;
-  fi
-else
-  if [[ "$TRAVIS_REPO_SLUG" = "conveyal/datatools-ui" ]] && [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "dev" ]]; then
-    export SHOULD_RUN_E2E=true;
-  fi
+if [[ "$TRAVIS_REPO_SLUG" = "ibi-group/datatools-ui" ]] && [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "dev" ]]; then
+  export SHOULD_RUN_E2E=true;
 fi
-
-# temporarily set SHOULD_RUN_E2E to true on PR for e2e branch
-# remove this just before merging to dev
-export SHOULD_RUN_E2E=true;
