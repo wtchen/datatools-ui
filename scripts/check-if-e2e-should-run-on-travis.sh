51172b5776fed461e022c270fd5a0aae63e8f04f
Landon Reed <landontreed@gmail.com>
2021-02-11 17:32:41 -0500

    ci(GitHub Actions): replace travis CI with GH actions

--- scripts/check-if-e2e-should-run-on-travis.sh
+++ scripts/check-if-e2e-should-run-on-travis.sh
@@ -1,14 +1,20 @@
 # Since the e2e tests take a while to run and it could present an inconvenience
 # to be making sure the e2e tests work on every single PR, only run the e2e
-# tests on Travis for PRs to master or on commits directly to dev or master
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  if [[ "$TRAVIS_BRANCH" = "master" ]]; then
-    export SHOULD_RUN_E2E=true;
-    echo 'Will run E2E tests because this is a PR to master'
-  fi
+# tests on CI for PRs to master or on commits directly to dev or master
+if [[ "$GITHUB_BASE_REF_SLUG" = "master" ]]; then
+  echo "SHOULD_RUN_E2E=true" >> $GITHUB_ENV && export SHOULD_RUN_E2E=true
+  echo 'Will run E2E tests because this is a PR to master'
 else
-  if [[ "$TRAVIS_REPO_SLUG" = "ibi-group/datatools-ui" ]] && [[ "$TRAVIS_BRANCH" = "master" || "$TRAVIS_BRANCH" = "dev" ]]; then
-    export SHOULD_RUN_E2E=true;
+  if [[ "$GITHUB_REPOSITORY" = "ibi-group/datatools-server" ]] && [[ "$GITHUB_REF_SLUG" = "master" || "$GITHUB_REF_SLUG" = "dev" || "$GITHUB_REF_SLUG" = "github-actions" ]]; then
+    echo "SHOULD_RUN_E2E=true" >> $GITHUB_ENV && export SHOULD_RUN_E2E=true
     echo 'Will run E2E tests because this is a commit to master or dev'
   fi
 fi
+
+if [[ "$SHOULD_RUN_E2E" != "true" ]]; then
+  echo 'Skipping E2E tests...'
+fi
+
+# FIXME: Re-enable e2e for conditions above.
+echo "SHOULD_RUN_E2E=false" >> $GITHUB_ENV && export SHOULD_RUN_E2E=true
+echo 'Overriding E2E. Temporarily forcing to be false...'
