f98cea482601ed5713d05649a545a86b64b65472
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-08-26 16:56:08 +0100

    undo debug writing

--- __tests__/e2e/server/launch.sh
+++ __tests__/e2e/server/launch.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Start the first process
-# java -jar otp-1.4.0-shaded.jar --server --autoScan --basePath /tmp/otp --insecure --router default &  
+java -jar otp-1.4.0-shaded.jar --server --autoScan --basePath /tmp/otp --insecure --router default &  
 
 # Start the second process
-echo "DEBUG:"
-echo "env:"
-env
-echo "conifg env"
-cat /config/env.yml
-
-# java -jar datatools-server-3.8.1-SNAPSHOT.jar /config/env.yml /config/server.yml & 
+java -jar datatools-server-3.8.1-SNAPSHOT.jar /config/env.yml /config/server.yml & 
 
 # Wait for any process to exit
 wait -n
